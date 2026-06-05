90609fa56231313356c59e1110c2152e58328561
Albazavr <aelbert.shapoval@mail.ru>
2026-06-05 22:52:34 +0300

    Taken back INSTANCE field

--- src/main/java/twilightforest/client/model/block/carpet/RoyalRagsModelLoader.java
+++ src/main/java/twilightforest/client/model/block/carpet/RoyalRagsModelLoader.java
@@ -8,6 +8,8 @@ import org.jetbrains.annotations.NotNull;
 import twilightforest.block.GenericModelLoader;
 
 public class RoyalRagsModelLoader extends GenericModelLoader<@NotNull UnbakedRoyalRagsModel> {
+	public static final RoyalRagsModelLoader INSTANCE = new RoyalRagsModelLoader();
+
 	public RoyalRagsModelLoader() {
 		super(UnbakedRoyalRagsModel::new);
 	}
