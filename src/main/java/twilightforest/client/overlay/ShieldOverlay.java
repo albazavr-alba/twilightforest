978c27bc8e89079acdb61440633b81e5701c4545
Albazavr <aelbert.shapoval@mail.ru>
2026-06-08 20:57:48 +0300

    Ported ShieldOverlay to 26.1.x

--- src/main/java/twilightforest/client/overlay/ShieldOverlay.java
+++ src/main/java/twilightforest/client/overlay/ShieldOverlay.java
@@ -2,7 +2,8 @@ package twilightforest.client.overlay;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.Nullable;
@@ -14,11 +15,11 @@ public class ShieldOverlay {
 
 	private static final Identifier FORTIFICATION_SHIELD_SPRITE = TwilightForestMod.prefix("fortification_shield");
 
-	public static void render(GuiGraphics graphics, Minecraft minecraft, Gui gui, @Nullable Player player) {
+	public static void render(GuiGraphicsExtractor graphics, Minecraft minecraft, Gui gui, @Nullable Player player) {
 		if (player != null && !minecraft.options.hideGui && (minecraft.gameMode.canHurtPlayer() || TFConfig.showFortificationShieldIndicatorInCreative) && player.hasData(TFDataAttachments.FORTIFICATION_SHIELDS) && player.getData(TFDataAttachments.FORTIFICATION_SHIELDS).shieldsLeft() > 0 && TFConfig.showFortificationShieldIndicator) {
 			int shieldCount = player.getData(TFDataAttachments.FORTIFICATION_SHIELDS).shieldsLeft();
 			for (int i = 0; i < Math.min(shieldCount, 10); i++) {
-				graphics.blitSprite(FORTIFICATION_SHIELD_SPRITE, graphics.guiWidth() / 2 - 91 + (i * 8), graphics.guiHeight() - gui.leftHeight, 9, 9);
+				graphics.blitSprite(RenderPipelines.GUI_TEXTURED, FORTIFICATION_SHIELD_SPRITE, graphics.guiWidth() / 2 - 91 + (i * 8), graphics.guiHeight() - gui.leftHeight, 9, 9);
 			}
 			gui.leftHeight += 10;
 		}
