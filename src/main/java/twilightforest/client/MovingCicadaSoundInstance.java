137fd8297a692d0c23af0af192b53c6da5d660a4
Albazavr <aelbert.shapoval@mail.ru>
2026-06-08 20:36:01 +0300

    Ported MovingCicadaSoundInstance to 26.1.x

--- src/main/java/twilightforest/client/MovingCicadaSoundInstance.java
+++ src/main/java/twilightforest/client/MovingCicadaSoundInstance.java
@@ -5,8 +5,6 @@ import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.neoforged.fml.ModList;
-import twilightforest.compat.curios.CuriosCompat;
 import twilightforest.config.TFConfig;
 import twilightforest.init.TFBlocks;
 import twilightforest.init.TFSounds;
@@ -27,7 +25,9 @@ public class MovingCicadaSoundInstance extends AbstractTickableSoundInstance {
 
 	@Override
 	public void tick() {
-		if (!this.wearer.isRemoved() && (this.wearer.getItemBySlot(EquipmentSlot.HEAD).is(TFBlocks.CICADA.asItem()) || this.isWearingCicadaCurio())) {
+		// Uncomment this when Curios mod is ready
+		// if (!this.wearer.isRemoved() && (this.wearer.getItemBySlot(EquipmentSlot.HEAD).is(TFBlocks.CICADA.asItem()) || this.isWearingCicadaCurio())) {
+		if (!this.wearer.isRemoved() && (this.wearer.getItemBySlot(EquipmentSlot.HEAD).is(TFBlocks.CICADA.asItem()))) {
 			this.x = (float) this.wearer.getX();
 			this.y = (float) this.wearer.getY();
 			this.z = (float) this.wearer.getZ();
@@ -35,13 +35,13 @@ public class MovingCicadaSoundInstance extends AbstractTickableSoundInstance {
 			this.stop();
 		}
 	}
-
-	private boolean isWearingCicadaCurio() {
-		if (ModList.get().isLoaded("curios")) {
-			return CuriosCompat.isCurioEquipped(this.wearer, stack -> stack.is(TFBlocks.CICADA.asItem()));
-		}
-		return false;
-	}
+	// Uncomment this when Curios mod is ready
+//	private boolean isWearingCicadaCurio() {
+//		if (ModList.get().isLoaded("curios")) {
+//			return CuriosCompat.isCurioEquipped(this.wearer, stack -> stack.is(TFBlocks.CICADA.asItem()));
+//		}
+//		return false;
+//	}
 
 	@Override
 	public boolean canPlaySound() {
