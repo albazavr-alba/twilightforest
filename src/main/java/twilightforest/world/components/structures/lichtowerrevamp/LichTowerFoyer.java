49de51729619671cba3431f1bddd6e19a6b08fbc
Albazavr <aelbert.shapoval@mail.ru>
2026-06-10 01:07:22 +0300

    Ported LichTowerFoyer to 26.1.x

--- src/main/java/twilightforest/world/components/structures/lichtowerrevamp/LichTowerFoyer.java
+++ src/main/java/twilightforest/world/components/structures/lichtowerrevamp/LichTowerFoyer.java
@@ -37,8 +37,8 @@ public final class LichTowerFoyer extends TwilightJigsawPiece implements PieceBe
 
 		LichTowerUtil.addDefaultProcessors(this.placeSettings);
 
-		this.putChest = compoundTag.getBoolean("put_chest");
-		this.chestSide = compoundTag.getBoolean("chest_side");
+		this.putChest = compoundTag.getBoolean("put_chest").get();
+		this.chestSide = compoundTag.getBoolean("chest_side").get();
 	}
 
 	public LichTowerFoyer(StructureTemplateManager structureManager, JigsawPlaceContext placeContext, boolean putChest, boolean chestSide) {
