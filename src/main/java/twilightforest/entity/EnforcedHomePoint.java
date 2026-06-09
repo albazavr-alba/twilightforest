85e17fde8cfc97a8ce0055e871d4a673f06e54a0
Albazavr <aelbert.shapoval@mail.ru>
2026-06-09 17:26:06 +0300

    Ported EnforcedHomePoint to 26.1.x

--- src/main/java/twilightforest/entity/EnforcedHomePoint.java
+++ src/main/java/twilightforest/entity/EnforcedHomePoint.java
@@ -1,10 +1,8 @@
 package twilightforest.entity;
 
+import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.GlobalPos;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.NbtOps;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.PathfinderMob;
@@ -12,44 +10,50 @@ import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.ValueInput;
 import net.minecraft.world.level.storage.ValueOutput;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import twilightforest.TwilightForestMod;
 import twilightforest.entity.ai.goal.AttemptToGoHomeGoal;
 import twilightforest.init.TFDimension;
 
 public interface EnforcedHomePoint {
-
 	default <T extends PathfinderMob & EnforcedHomePoint> void addRestrictionGoals(T entity, GoalSelector selector) {
 		selector.addGoal(5, new AttemptToGoHomeGoal<>(entity, 1.25D));
 	}
 
 	default void saveHomePointToNbt(ValueOutput tag) {
 		if (this.getRestrictionPoint() != null) {
-			GlobalPos.CODEC.encodeStart(NbtOps.INSTANCE, this.getRestrictionPoint()).resultOrPartial(TwilightForestMod.LOGGER::error).ifPresent(tag1 -> tag.put("HomePos", tag1));
+			tag.store("HomePos", GlobalPos.CODEC, this.getRestrictionPoint());
 		}
 	}
 
 	default void loadHomePointFromNbt(ValueInput tag) {
-		//properly load old home points, just assume theyre set in TF
-		if (tag.contains("Home", 9)) {
-			ListTag nbttaglist = tag.getList("Home", 6);
-			double hx = nbttaglist.getDouble(0);
-			double hy = nbttaglist.getDouble(1);
-			double hz = nbttaglist.getDouble(2);
-			this.setRestrictionPoint(GlobalPos.of(TFDimension.DIMENSION_KEY, BlockPos.containing(hx, hy, hz)));
+		var modernPos = tag.read("HomePos", GlobalPos.CODEC);
+
+		if (modernPos.isPresent()) {
+			this.setRestrictionPoint(modernPos.get());
 		} else {
-			if (tag.contains("HomePos")) {
-				this.setRestrictionPoint(GlobalPos.CODEC.parse(NbtOps.INSTANCE, tag.get("HomePos")).resultOrPartial(TwilightForestMod.LOGGER::error).orElse(null));
-			}
+			tag.read("Home", Codec.DOUBLE.listOf()).ifPresent(doubleList -> {
+				if (doubleList.size() >= 3) {
+					double hx = doubleList.get(0);
+					double hy = doubleList.get(1);
+					double hz = doubleList.get(2);
+
+					this.setRestrictionPoint(GlobalPos.of(
+						TFDimension.DIMENSION_KEY,
+						BlockPos.containing(hx, hy, hz)
+					));
+				}
+			});
 		}
 	}
+
 
 	default boolean isMobWithinHomeArea(Entity entity) {
 		if (!this.isRestrictionPointValid(entity.level().dimension())) return true;
 		return this.getRestrictionPoint().pos().distSqr(entity.blockPosition()) < (double) (this.getHomeRadius() * this.getHomeRadius());
 	}
 
-	default boolean isRestrictionPointValid(ResourceKey<Level> currentMobLevel) {
+	default boolean isRestrictionPointValid(ResourceKey<@NotNull Level> currentMobLevel) {
 		return this.getRestrictionPoint() != null && this.getRestrictionPoint().dimension().equals(currentMobLevel);
 	}
 
