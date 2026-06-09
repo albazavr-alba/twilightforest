536fe580c70222d71dcd30916a301a7cdcb9d5e7
Albazavr <aelbert.shapoval@mail.ru>
2026-06-09 22:16:27 +0300

    Ported ProtectionBox to 26.1.x

--- src/main/java/twilightforest/entity/ProtectionBox.java
+++ src/main/java/twilightforest/entity/ProtectionBox.java
@@ -1,12 +1,15 @@
 package twilightforest.entity;
 
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 import twilightforest.init.TFEntities;
 
 public class ProtectionBox extends Entity {
@@ -31,7 +34,8 @@ public class ProtectionBox extends Entity {
 
 		this.sbb = sbb;
 
-		this.moveTo(sbb.minX(), sbb.minY(), sbb.minZ(), 0.0F, 0.0F);
+		this.setPos(sbb.minX(), sbb.minY(), sbb.minZ());
+		this.setRot(0.0F, 0.0F);
 
 		this.sizeX = sbb.getXSpan();
 		this.sizeY = sbb.getYSpan();
@@ -63,15 +67,17 @@ public class ProtectionBox extends Entity {
 	}
 
 	@Override
-	protected void defineSynchedData(SynchedEntityData.Builder builder) {
-	}
+	protected void defineSynchedData(SynchedEntityData.Builder builder) {}
 
 	@Override
-	protected void readAdditionalSaveData(CompoundTag compound) {
-	}
+	protected void readAdditionalSaveData(ValueInput compound) {}
 
 	@Override
-	protected void addAdditionalSaveData(CompoundTag compound) {
+	protected void addAdditionalSaveData(ValueOutput compound) {}
+
+	@Override
+	public boolean hurtServer(ServerLevel serverLevel, DamageSource damageSource, float v) {
+		return false;
 	}
 
 	@Override
