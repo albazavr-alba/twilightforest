75154ceb690a30a0b63e38189a7bdd1f77d45bef
Albazavr <aelbert.shapoval@mail.ru>
2026-06-09 14:08:35 +0300

    Ported ArmorUtil to 26.1.x

--- src/main/java/twilightforest/util/ArmorUtil.java
+++ src/main/java/twilightforest/util/ArmorUtil.java
@@ -1,24 +1,26 @@
 package twilightforest.util;
 
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.DyedItemColor;
 import tamaized.beanification.Component;
 import twilightforest.init.TFDataComponents;
-import twilightforest.item.ArcticArmorItem;
-
-import java.util.OptionalInt;
 
 @Component
 public class ArmorUtil {
-
 	public float getShroudedArmorPercentage(LivingEntity entity) {
 		int shroudedArmor = 0;
 		int nonShroudedArmor = 0;
 
-		for (ItemStack stack : entity.getArmorSlots()) {
-			if (!stack.isEmpty() && stack.get(TFDataComponents.EMPERORS_CLOTH) != null) {
-				shroudedArmor++;
+		EquipmentSlot[] armorSlots = {EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET};
+
+		for (EquipmentSlot slot : armorSlots) {
+			ItemStack stack = entity.getItemBySlot(slot);
+
+			if (!stack.isEmpty()) {
+				if (stack.has(TFDataComponents.EMPERORS_CLOTH)) {
+					shroudedArmor++;
+				}
 			}
 
 			nonShroudedArmor++;
@@ -26,5 +28,5 @@ public class ArmorUtil {
 
 		return nonShroudedArmor > 0 && shroudedArmor > 0 ? (float) shroudedArmor / (float) nonShroudedArmor : 0.0F;
 	}
-
 }
+
