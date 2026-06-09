96813c1384301b6b53ceeebeaf51f8589f1853e3
Albazavr <aelbert.shapoval@mail.ru>
2026-06-09 17:34:40 +0300

    Ported type tags to 26.1.x

--- src/main/java/twilightforest/tags/TFBlockEntityTypeTags.java
+++ src/main/java/twilightforest/tags/TFBlockEntityTypeTags.java
@@ -1,16 +1,17 @@
 package twilightforest.tags;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import org.jetbrains.annotations.NotNull;
 
 public class TFBlockEntityTypeTags {
 
-	public static final TagKey<BlockEntityType<?>> RELOCATION_NOT_SUPPORTED = create("c", "relocation_not_supported");
-	public static final TagKey<BlockEntityType<?>> IMMOVABLE = create("c", "immovable");
+	public static final TagKey<@NotNull BlockEntityType<?>> RELOCATION_NOT_SUPPORTED = create("c", "relocation_not_supported");
+	public static final TagKey<@NotNull BlockEntityType<?>> IMMOVABLE = create("c", "immovable");
 
-	private static TagKey<BlockEntityType<?>> create(String modid, String tagName) {
-		return TagKey.create(Registries.BLOCK_ENTITY_TYPE, ResourceLocation.fromNamespaceAndPath(modid, tagName));
+	private static TagKey<@NotNull BlockEntityType<?>> create(String modid, String tagName) {
+		return TagKey.create(Registries.BLOCK_ENTITY_TYPE, Identifier.fromNamespaceAndPath(modid, tagName));
 	}
 }
