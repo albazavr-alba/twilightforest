50cb0cc1dc2bc32c79faace506b1004174c0d547
Albazavr <aelbert.shapoval@mail.ru>
2026-06-10 00:05:48 +0300

    Ported TransformationParticle to 26.1.x

--- src/main/java/twilightforest/client/particle/TransformationParticle.java
+++ src/main/java/twilightforest/client/particle/TransformationParticle.java
@@ -7,6 +7,7 @@ import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.phys.AABB;
+import org.jetbrains.annotations.NotNull;
 
 public class TransformationParticle extends SingleQuadParticle {
 	private final double xStart;
@@ -82,16 +83,15 @@ public class TransformationParticle extends SingleQuadParticle {
 		}
 	}
 
-	public record Factory(SpriteSet sprite) implements ParticleProvider<SimpleParticleType> {
+	public record Factory(SpriteSet sprite) implements ParticleProvider<@NotNull SimpleParticleType> {
 		@Override
 		public Particle createParticle(SimpleParticleType particleType, ClientLevel level, double x, double y, double z, double x2, double y2, double z2, RandomSource random) {
-			TransformationParticle sortingParticle = new TransformationParticle(level, x, y, z, x2, y2, z2, this.sprite.get(random));
-			return sortingParticle;
+			return new TransformationParticle(level, x, y, z, x2, y2, z2, this.sprite.get(random));
 		}
 	}
 
 	@Override
-	public AABB getRenderBoundingBox(float partialTicks) {
+	public AABB getBoundingBox() {
 		return AABB.INFINITE;
 	}
 }
