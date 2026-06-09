c4bd821b0f112fecd1dff01ad1d49d14c0637dfb
Albazavr <aelbert.shapoval@mail.ru>
2026-06-09 14:22:13 +0300

    Ported FoliageColorHandler to 26.1.x

--- src/main/java/twilightforest/client/FoliageColorHandler.java
+++ src/main/java/twilightforest/client/FoliageColorHandler.java
@@ -2,12 +2,12 @@ package twilightforest.client;
 
 import com.google.common.collect.MapMaker;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.event.entity.EntityLeaveLevelEvent;
+import org.jetbrains.annotations.NotNull;
 import tamaized.beanification.Autowired;
 import tamaized.beanification.Component;
 import tamaized.beanification.PostConstruct;
@@ -23,7 +23,7 @@ public final class FoliageColorHandler {
 	@Autowired
 	private BiomeColorAlgorithms biomeColorAlgorithms;
 
-	private final Map<ResourceKey<Biome>, Handler> REGISTRY = new HashMap<>() {{
+	private final Map<ResourceKey<@NotNull Biome>, Handler> REGISTRY = new HashMap<>() {{
 		put(TFBiomes.SPOOKY_FOREST, (o, x, z) -> biomeColorAlgorithms.spookyFoliage(x, z));
 		put(TFBiomes.ENCHANTED_FOREST, (o, x, z) -> biomeColorAlgorithms.enchanted(o, (int) x, (int) z));
 		put(TFBiomes.DARK_FOREST_CENTER, (o, x, z) -> biomeColorAlgorithms.darkForestCenterFoliage(x, z));
@@ -47,15 +47,11 @@ public final class FoliageColorHandler {
 		if (handler == null) {
 			handler = REGISTRY.getOrDefault(
 				Minecraft.getInstance().level == null ? null :
-					Minecraft.getInstance().level.registryAccess().registryOrThrow(Registries.BIOME).getResourceKey(biome).orElse(null),
+					Minecraft.getInstance().level.registryAccess().lookupOrThrow(Registries.BIOME).getResourceKey(biome).orElse(null),
 				Handler.DEFAULT);
 			HANDLES.put(biome, handler);
 		}
 		return handler.apply(o, x, z);
-	}
-
-	public static int getTintColorAtPosition(BlockPos pos) {
-
 	}
 
 	@FunctionalInterface
