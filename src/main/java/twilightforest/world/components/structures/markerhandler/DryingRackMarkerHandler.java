8f42665640fac0565848a69e4c18c98d7990d531
Albazavr <aelbert.shapoval@mail.ru>
2026-06-10 00:34:42 +0300

    Ported DryingRackMarkerHandler to 26.1.x

--- src/main/java/twilightforest/world/components/structures/markerhandler/DryingRackMarkerHandler.java
+++ src/main/java/twilightforest/world/components/structures/markerhandler/DryingRackMarkerHandler.java
@@ -14,11 +14,12 @@ import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.storage.loot.LootTable;
+import org.jetbrains.annotations.NotNull;
 import twilightforest.block.DryingRackBlock;
 import twilightforest.block.entity.DryingRackBlockEntity;
 import twilightforest.init.custom.TemplateMarkerHandlers;
 
-public record DryingRackMarkerHandler(BlockStateProvider provider, ResourceKey<LootTable> lootTable) implements TemplateMarkerHandler {
+public record DryingRackMarkerHandler(BlockStateProvider provider, ResourceKey<@NotNull LootTable> lootTable) implements TemplateMarkerHandler {
 	public static final MapCodec<DryingRackMarkerHandler> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
 		BlockStateProvider.CODEC.fieldOf("drying_rack_block").forGetter(DryingRackMarkerHandler::provider),
 		ResourceKey.codec(Registries.LOOT_TABLE).fieldOf("loot_table").forGetter(DryingRackMarkerHandler::lootTable)
@@ -26,7 +27,7 @@ public record DryingRackMarkerHandler(BlockStateProvider provider, ResourceKey<L
 
 	@Override
 	public boolean handleDataMarker(String label, BlockPos pos, WorldGenLevel level, RandomSource random, BoundingBox chunkBounds, ChunkGenerator chunkGen, Rotation rotation) {
-		BlockState state = this.provider.getState(random, pos).rotate(rotation);
+		BlockState state = this.provider.getState(level, random, pos).rotate(level, pos, rotation);
 
 		if (!(state.getBlock() instanceof DryingRackBlock) || !level.setBlock(pos, state, Block.UPDATE_ALL)) {
 			return false;
