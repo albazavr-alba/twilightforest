168f8beb9f30e32d156d4227918808481c7decd1
Albazavr <aelbert.shapoval@mail.ru>
2026-06-09 14:10:07 +0300

    Ported BurntThornsBlock to 26.1.x

--- src/main/java/twilightforest/block/BurntThornsBlock.java
+++ src/main/java/twilightforest/block/BurntThornsBlock.java
@@ -2,10 +2,12 @@ package twilightforest.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.InsideBlockEffectApplier;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -15,7 +17,6 @@ import net.minecraft.world.level.pathfinder.PathType;
 import org.jspecify.annotations.Nullable;
 
 public class BurntThornsBlock extends ThornsBlock {
-
 	public BurntThornsBlock(Properties properties) {
 		super(properties);
 	}
@@ -27,7 +28,7 @@ public class BurntThornsBlock extends ThornsBlock {
 	}
 
 	@Override
-	public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
+	public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity, InsideBlockEffectApplier effectApplier, boolean isPrecise) {
 		// dissolve
 		if (!level.isClientSide() && (entity instanceof LivingEntity || entity instanceof Projectile)) {
 			level.destroyBlock(pos, false);
@@ -35,7 +36,7 @@ public class BurntThornsBlock extends ThornsBlock {
 	}
 
 	@Override
-	public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+	public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, ItemStack toolStack, boolean willHarvest, FluidState fluid) {
 		state.getBlock().playerWillDestroy(level, pos, state, player);
 		return level.setBlock(pos, fluid.createLegacyBlock(), level.isClientSide() ? Block.UPDATE_ALL_IMMEDIATE : Block.UPDATE_ALL);
 	}
