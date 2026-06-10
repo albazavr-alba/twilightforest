523a6b6d913036f13446aa7c55d81c4a62a784f4
Albazavr <aelbert.shapoval@mail.ru>
2026-06-10 03:46:47 +0300

    Fixed issues

--- src/main/java/twilightforest/block/AbstractSkullCandleBlock.java
+++ src/main/java/twilightforest/block/AbstractSkullCandleBlock.java
@@ -232,7 +232,7 @@ public abstract class AbstractSkullCandleBlock extends BaseEntityBlock implement
 
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(LIGHTING, CANDLES);
+		builder.add(LIGHTING, CANDLES, POWERED);
 	}
 
 	@Nullable
