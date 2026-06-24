44775a07ec4c38801ad100d16b27a372abfa1b22
Albazavr <aelbert.shapoval@mail.ru>
2026-06-24 03:17:50 +0300

    Fixed MovingCicadaSoundInstance for 26.1.x

--- src/main/java/twilightforest/client/MovingCicadaSoundInstance.java
+++ src/main/java/twilightforest/client/MovingCicadaSoundInstance.java
@@ -5,6 +5,8 @@ import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.fml.ModList;
+import twilightforest.compat.curios.CuriosCompat;
 import twilightforest.config.TFConfig;
 import twilightforest.init.TFBlocks;
 import twilightforest.init.TFSounds;
@@ -25,23 +27,23 @@ public class MovingCicadaSoundInstance extends AbstractTickableSoundInstance {
 
 	@Override
 	public void tick() {
-		// Uncomment this when Curios mod is ready
-		// if (!this.wearer.isRemoved() && (this.wearer.getItemBySlot(EquipmentSlot.HEAD).is(TFBlocks.CICADA.asItem()) || this.isWearingCicadaCurio())) {
-		if (!this.wearer.isRemoved() && (this.wearer.getItemBySlot(EquipmentSlot.HEAD).is(TFBlocks.CICADA.asItem()))) {
-			this.x = (float) this.wearer.getX();
-			this.y = (float) this.wearer.getY();
-			this.z = (float) this.wearer.getZ();
-		} else {
-			this.stop();
-		}
+		 if (!this.wearer.isRemoved() && (this.wearer.getItemBySlot(EquipmentSlot.HEAD).is(TFBlocks.CICADA.asItem()) || this.isWearingCicadaCurio())) {
+			 if (!this.wearer.isRemoved() && (this.wearer.getItemBySlot(EquipmentSlot.HEAD).is(TFBlocks.CICADA.asItem()))) {
+				 this.x = (float) this.wearer.getX();
+				 this.y = (float) this.wearer.getY();
+				 this.z = (float) this.wearer.getZ();
+			 } else {
+				 this.stop();
+			 }
+		 }
 	}
-	// Uncomment this when Curios mod is ready
-//	private boolean isWearingCicadaCurio() {
-//		if (ModList.get().isLoaded("curios")) {
-//			return CuriosCompat.isCurioEquipped(this.wearer, stack -> stack.is(TFBlocks.CICADA.asItem()));
-//		}
-//		return false;
-//	}
+
+	private boolean isWearingCicadaCurio() {
+		if (ModList.get().isLoaded("curios")) {
+			return CuriosCompat.isCurioEquipped(this.wearer, stack -> stack.is(TFBlocks.CICADA.asItem()));
+		}
+		return false;
+	}
 
 	@Override
 	public boolean canPlaySound() {
